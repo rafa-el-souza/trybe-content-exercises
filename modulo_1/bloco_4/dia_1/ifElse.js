1508c1a4d62e6019dd7056ad527ef1f306519f8e
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-27 18:51:55 -0300

    Refactoring to use functions | ifElse.js

--- modulo_1/bloco_4/dia_1/ifElse.js
+++ modulo_1/bloco_4/dia_1/ifElse.js
@@ -1,11 +1,16 @@
-let nota = Math.random() * (100 - 1) + 1; //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random#gerando_um_n%C3%BAmero_aleat%C3%B3rio_entre_dois_valores
-
-console.log(nota);
-
-if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!")
-} else if (nota < 80 && nota >= 60) {
-    console.log("Você está na nossa lista de espera");
-} else {
-    console.log("Você foi reprovada(o)");
+function generateScore(min, max) {
+    let score = Math.ceil(Math.random() * (max - min) + min) // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random#gerando_um_n%C3%BAmero_aleat%C3%B3rio_entre_dois_valores
+    console.log(`Sua nota foi ${score}`);
+    return score;
 }
+function scoreToConcept(score) {
+    if (score >= 80) {
+        console.log("Parabéns, você foi aprovada(o)!")
+    } else if (score < 80 && score >= 60) {
+        console.log("Você está na nossa lista de espera");
+    } else {
+        console.log("Você foi reprovada(o)");
+    }
+}
+
+scoreToConcept(generateScore(1, 100));
