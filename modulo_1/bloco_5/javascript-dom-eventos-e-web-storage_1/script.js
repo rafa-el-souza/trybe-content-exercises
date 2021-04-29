b06dbc7594a298ce7637c37102625a9057dcfb6d
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-04-29 17:48:30 -0300

    Completed exercise | DOManipulation.html

---
+++ modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,11 @@
+document.getElementById('header-container').style.color = '#2A2D43';
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#EE6C4D';
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#2A2D43';
+
+document.getElementById('container').style.backgroundColor = '#519872';
+
+document.querySelector("#footer-container").style.backgroundColor = '#FCBA04'
+
+document.querySelector('.no-emergency-tasks div').style.backgroundColor = '#247BA0'
